d9eb5278b3da39364191cb6cb7976fe60f8c6553
Mark <markloercher@gmail.com>
2022-07-30 01:59:10 -0400

    displaying imgs

--- src/components/Article.js
+++ src/components/Article.js
@@ -5,6 +5,8 @@ function Article({data}) {
   return (
     <div>
        <h1>{data.title}</h1>
+       <a href={data.url}><img className="news-img" src={data.urlToImage} alt={data.urlToImage}></img></a>
+       <p>{data.description}</p>
     </div>
   )
 }
